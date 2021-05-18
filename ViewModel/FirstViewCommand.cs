97a89f353b210cd07bdd6d7b5768722c3cc5df91
asrock <asrock@YBK>
2021-05-18 13:44:21 +0900

    1

---
+++ ViewModel/FirstViewCommand.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace tabControl1.ViewModel
+{
+    class LoadCSVBtnCommand : ICommand
+    {
+        private FirstViewViewModel fvm;
+        public event EventHandler CanExecuteChanged;
+        public LoadCSVBtnCommand(FirstViewViewModel fvm)
+        {
+            this.fvm = fvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            //로드파일로직구현
+            LoadFile();
+        }
+
+
+
+        private static double LoadFile(double first, string symbol, double second)             //first,second는 첫,둘째값, symbol은 연산기호
+        {
+            double result = 0;                                                                  //result(결과값) 정의 및 초기화
+
+
+            if (symbol.Equals("*"))  //곱셈
+            {
+                result = first * second;
+
+            }
+            else if (symbol.Equals("/")) //나눗셈
+            {
+                result = first / second;
+            }
+            else if (symbol.Equals("+")) //덧셈
+            {
+                result = first + second;
+            }
+            else if (symbol.Equals("-")) //뺄셈
+            {
+                result = first - second;
+            }
+
+            return result;   //calculate결과물, 결과값 반환
+        }
+
+    }
+
+    class SaveCSVBtnCommand : ICommand
+    {
+        private FirstViewViewModel fvm;
+        public event EventHandler CanExecuteChanged;
+        public SaveCSVBtnCommand(FirstViewViewModel fvm)
+        {
+            this.fvm = fvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            //로드파일로직구현
+        }
+    }
+
+    class AddRowBtnCommand : ICommand
+    {
+        private FirstViewViewModel fvm;
+        public event EventHandler CanExecuteChanged;
+        public AddRowBtnCommand(FirstViewViewModel fvm)
+        {
+            this.fvm = fvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            //로드파일로직구현
+        }
+    }
+
+    class DeleteRowBtnCommand : ICommand
+    {
+        private FirstViewViewModel fvm;
+        public event EventHandler CanExecuteChanged;
+        public DeleteRowBtnCommand(FirstViewViewModel fvm)
+        {
+            this.fvm = fvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            //로드파일로직구현
+        }
+    }
+
+
+
+
+
+
+
+}
--- ViewModel/IMyTabContentViewModel.cs
+++ ViewModel/IMyTabContentViewModel.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace tabControl1.ViewModel
 {
     public interface IMyTabContentViewModel { }
-    public class FirstViewViewModel : IMyTabContentViewModel { }
-    public class SecondViewViewModel : IMyTabContentViewModel { }
+    public class FirstViewViewModel : BaseViewModel, IMyTabContentViewModel
+    {
+        public FirstViewViewModel()
+        {
+            this.LoadCSVBtnCommand = new LoadCSVBtnCommand(this);
+            this.SaveCSVBtnCommand = new SaveCSVBtnCommand(this);
+            this.AddRowBtnCommand = new AddRowBtnCommand(this);
+            this.DeleteRowBtnCommand = new DeleteRowBtnCommand(this);
 
+        }
+
+        string displayText = "";
+        //계산기의 출력창과 바인딩된 속성
+        public string DisplayText
+        {
+            internal set
+            {
+                if (displayText != value)
+                {
+                    displayText = value;
+                    OnPropertyChanged("DisplayText");
+                }
+            }
+            get { return displayText; }
+        }
+
+
+        public ICommand LoadCSVBtnCommand { protected set; get;}
+        public ICommand SaveCSVBtnCommand { protected set; get; }
+        public ICommand AddRowBtnCommand { protected set; get; }
+        public ICommand DeleteRowBtnCommand { protected set; get; }
+
+
+
+
+
+    }
+
+
+
+
+
+
+    public class SecondViewViewModel : IMyTabContentViewModel 
+    {
+
+    }
 
 }
